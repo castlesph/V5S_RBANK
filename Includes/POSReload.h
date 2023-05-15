f7af77cc786102795b121c2150a29083ec61e79b
castech-renhong <skyz@castech.asia>
2023-05-15 09:59:09 +0800

    Remove all file and doing import all new updated files later

--- Includes/POSReload.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef ___POS_RELOAD___
-#define ___POS_RELOAD___
-
-int inCTOS_ReloadFlowProcess(void);
-int inCTOS_RELOAD(void);
-
-#endif //end ___POS_SALE___
-
--- Includes/POSVoid.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef ___POS_VOID___
-#define ___POS_VOID___
-
-int inCTOS_VOID(void);
-int inCTOS_VoidFlowProcess(void);
-
-#endif //end ___POS_VOID___
-
